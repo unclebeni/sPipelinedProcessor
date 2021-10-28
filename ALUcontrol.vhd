659f7b9f521e9e6ac38ddae0a4e11b639a874121
Cole Hunt <huntcr2001@gmail.com>
2021-10-28 15:03:03 -0500

    Setup ALU output control lines

--- ALUcontrol.vhd
+++ ALUcontrol.vhd
@@ -12,11 +12,11 @@ use IEEE.std_logic_1164.all;
 
 entity decoder is
 
-	   port(i_ALUop  		: in std_logic_vector(4 downto 0);
-		o_ALUShiftDir 		: out std_logic;
-	     	o_ALUShirtArithmetic	: out std_logic;
-	     	o_ALUAddSub		: out std_logic;
-		o_ALUMuxCtrl		: out std_logic_vector(3 downto 0);
+	   port(i_ALUop  				: in std_logic_vector(4 downto 0);
+			o_ALUShiftDir 			: out std_logic;
+	     	o_ALUShiftArithmetic	: out std_logic;
+	     	o_ALUAddSub				: out std_logic;
+			o_ALUMuxCtrl			: out std_logic_vector(2 downto 0);
 		);
 
 end decoder;
@@ -27,19 +27,44 @@ begin
 
 with i_ALUop select
 	o_ALUAddSub <=
-		"1" when "0000",
-		"0" when others;
+		"1" when "0000", -- Add
+		"0" when "0001", -- Sub
+		"X" when others;
 
 with i_ALUop select
 	o_ALUShiftDir <=
-		"1" when "0111",
-		"0" when others;
+		"1" when "0111", -- Shift Left
+		"0" when "1000", -- Shift Right
+		"0" when "1001"; -- Shift Right Arithmetic
+		"X" when others;
 
 with i_ALUop select
 	o_ALUShiftArithmetic <=
-		"1" when "0111",
+		"1" when "1001", -- Shift Arithmetic
 		"0" when others;
 
+with i_ALUop select
+	o_ALUMuxCtrl <=
+		-- Adder/Subtractor
+		"000" when "0000", -- Add
+		"000" when "0001", -- Sub
+
+		-- AND
+		"001" when "0010",
+
+		-- OR
+		"010" when "0011",
+
+		-- OR + INV
+		"011" when "0101",
+
+		-- XOR
+		"100" when "0100",
+
+		-- Shifter
+		"101" when "0111", -- Shift Left
+		"101" when "1000", -- Shift Right
+		"101" when "1001"; -- Shift Right Arithmetic
 
 
 end data;
