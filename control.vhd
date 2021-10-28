9a40aec77aa72fbab30469ddbaa595f7b2912606
Cole Hunt <huntcr2001@gmail.com>
2021-10-28 15:57:51 -0500

    Clean control whitespace

--- control.vhd
+++ control.vhd
@@ -10,17 +10,17 @@ use IEEE.std_logic_1164.all;
 
 entity decoder is
 
-	   port(i_opCode  	: in std_logic_vector(5 downto 0);
-			o_RegDest 	: out std_logic; -- '1' when using R format instruction
-	    	o_ALUSrc	: out std_logic; -- '1' for immediate value operations
-	     	o_MemtoReg	: out std_logic; -- '1' for load word
-			o_RegWrite	: out std_logic; -- '1' for storing to register
-			o_MemRead	: out std_logic; -- '1' for reading memory
-			o_MemWrite	: out std_logic; -- '1' for store word in memory
-			o_branch	: out std_logic; -- '1' for branch and jump operations
-			o_WriteRa	: out std_logic; -- '1' when using jal
-			o_signed	: out std_logic; -- '1' when adding or subtracting a signed number
-	     	o_ALUop	: out std_logic_vector(3 downto 0)); -- ALU op code
+	port(i_opCode  	: in std_logic_vector(5 downto 0);
+		o_RegDest 	: out std_logic; -- '1' when using R format instruction
+	   	o_ALUSrc	: out std_logic; -- '1' for immediate value operations
+    	o_MemtoReg	: out std_logic; -- '1' for load word
+		o_RegWrite	: out std_logic; -- '1' for storing to register
+		o_MemRead	: out std_logic; -- '1' for reading memory
+		o_MemWrite	: out std_logic; -- '1' for store word in memory
+		o_branch	: out std_logic; -- '1' for branch and jump operations
+		o_WriteRa	: out std_logic; -- '1' when using jal
+		o_signed	: out std_logic; -- '1' when adding or subtracting a signed number
+	    o_ALUop	: out std_logic_vector(3 downto 0)); -- ALU op code
 
 end decoder;
 
