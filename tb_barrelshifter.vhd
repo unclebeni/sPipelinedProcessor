f65c5bc8f9739fa211df4a443a950ebddec9e850
bmoeckly <bmoeckly@vlinux-09.ece.iastate.edu>
2021-11-04 13:27:01 -0500

    testable barrel shifter implementation

---
+++ tb_barrelshifter.vhd
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------
+-- Benito Moeckly
+-- Testbench for my 32 bit barrel shifter
+--
+-- CREATED ON: 11.4.21
+-------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
