387f2344ab30f9850b30a4af0d695ab527e125c7
Sidney Stowe <92547248+Sidfour4@users.noreply.github.com>
2021-11-06 16:41:03 -0500

    Add files via upload

---
+++ FullAdderSid.vhd
@@ -0,0 +1,70 @@
+-------------------------------------------------------------------------
+-- Sidney Stowe
+-- CPR E 381
+-- FullAdder
+-------------------------------------------------------------------------
+
+-- FullAdder.vhd
+-------------------------------------------------------------------------
+-- DESCRIPTION: This file contains an implementation of a Full Adder
+-------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity FullAdderSid is
+	port(i_A	: in std_logic;
+	     i_B	: in std_logic;
+	     i_C	: in std_logic;
+	     o_S	: out std_logic;
+	     o_C	: out std_logic);
+end FullAdderSid;
+
+architecture structural of FullAdder is
+
+component invg is
+	port(i_A	: in std_logic;
+	     o_F	: out std_logic);
+end component;
+
+component andg2 is
+	port(i_A	: in std_logic;
+	     i_B	: in std_logic;
+	     o_F	: out std_logic);
+end component;
+
+component org2 is
+	port(i_A	: in std_logic;
+	     i_B	: in std_logic;
+	     o_F	: out std_logic);
+end component;
+
+signal A, B, C, nA, nB, nC, xor1, xor1and1, xor1and2, nxor1, xor2, xor2and1, 
+xor2and2, coand1, coand2, coor	: std_logic;
+
+begin
+	A <= i_A;
+	B <= i_B;
+	C <= i_C;
+	
+	NOTA: invg port map(i_A => A, o_F => nA);
+	NOTB: invg port map(i_A => B, o_F => nB);
+	NOTC: invg port map(i_A => C, o_F => nC);
+
+	MXOR1AND1: andg2 port map(i_A => A, i_B => nB, o_F => xor1and1);
+	MXOR1AND2: andg2 port map(i_A => nA, i_B => B, o_F => xor1and2);
+	MXOR1OR: org2 port map(i_A => xor1and1, i_B => xor1and2, o_F => xor1);
+	MNXOR1: invg port map(i_A => xor1, o_F => nxor1);
+
+	MXOR2AND1: andg2 port map(i_A => xor1, i_B => nC, o_F => xor2and1);
+	MXOR2AND2: andg2 port map(i_A => nxor1, i_B => C, o_F => xor2and2);
+	MXOR2OR: org2 port map(i_A => xor2and1, i_B => xor2and2, o_F => xor2);
+
+	MCOAND1: andg2 port map(i_A => xor1, i_B => C, o_F => coand1);
+	MCOAND2: andg2 port map(i_A => A, i_B => B, o_F => coand2);
+	MCOOR: org2 port map(i_A => coand1, i_B => coand2, o_F => coor);
+
+	o_S <= xor2;
+	o_C <= coor;
+
+end structural;
--- RippleCarryAdder.vhd
+++ RippleCarryAdder.vhd
@@ -23,7 +23,7 @@ end RippleCarryAdder;
 
 architecture structural of RippleCarryAdder is
 
-component FullAdder is
+component FullAdderSid is
 	port(i_A	: in std_logic;
 	     i_B	: in std_logic;
 	     i_C	: in std_logic;
@@ -34,10 +34,10 @@ end component;
 signal Cip	: std_logic_vector(N-1 downto 0);
 
 begin
-	ADDER0: FullAdder port map(i_A => i_A(0), i_B => i_B(0), i_C => i_C, o_S => o_S(0), o_C => Cip(0));
+	ADDER0: FullAdderSid port map(i_A => i_A(0), i_B => i_B(0), i_C => i_C, o_S => o_S(0), o_C => Cip(0));
 
 	G_NBit_RCA: for i in 1 to N-1 generate
-		ADDERI: FullAdder port map(i_A => i_A(i), i_B => i_B(i), i_C => Cip(i-1), o_S => o_S(i), o_C => Cip(i));
+		ADDERI: FullAdderSid port map(i_A => i_A(i), i_B => i_B(i), i_C => Cip(i-1), o_S => o_S(i), o_C => Cip(i));
 	end generate G_NBit_RCA;
 	o_C <= Cip(N-1);
 end structural;
