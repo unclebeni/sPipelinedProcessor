357eccba7856f38b4bf3b43a5a934945bf232898
crhunt <crhunt@co2050-02.ece.iastate.edu>
2021-11-11 15:07:06 -0600

    Rebuild src dir

---
+++ test/tb_ALU.vhd
@@ -0,0 +1,262 @@
+-------------------------------------------------------------------
+-- Benito Moeckly
+-- Testbench for ALU
+--
+-- CREATED ON: 11.4.21
+-------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+-- Usually name your testbench similar to below for clarity tb_<name>
+entity tb_ALU is
+  generic(gCLK_HPER   : time := 10 ns);   -- Generic for half of the clock cycle period
+end tb_ALU;
+
+architecture behavior of tb_ALU is
+
+-- Define the total clock period time
+constant cCLK_PER  : time := gCLK_HPER * 2;
+
+
+component ALU is
+port(
+	i_Adata		: in std_logic_vector(32-1 downto 0);
+	i_Bdata		: in std_logic_vector(32-1 downto 0);
+	i_ALUShiftDir	: in std_logic;
+	i_ALUShiftArithmetic	: in std_logic;
+	i_ALUAddSub		: in std_logic;
+	i_ALUMuxCtrl	: in std_logic_vector(3-1 downto 0);
+	i_shamt		:	in std_logic_vector(5-1 downto 0);
+	i_signed	:	in std_logic;
+	o_equal			: out std_logic;
+	o_carryout	:	out std_logic;
+	o_overflow	:	out std_logic;
+	o_result	: out std_logic_vector(32-1 downto 0));
+end component; 
+--signals
+
+signal s_CLK : std_logic := '0';
+signal s_Adata	: std_logic_vector(32-1 downto 0) := (others => '0');
+signal s_Bdata	: std_logic_vector(32-1 downto 0) := (others => '0');
+signal s_ALUShiftDir	:	std_logic := '0';
+signal s_ALUSHiftArithmetic	:	std_logic := '0';
+signal s_ALUAddSub	:	std_logic := '0';
+signal s_ALUMuxCtrl	:	std_logic_vector(3-1 downto 0) := (others => '0');
+signal s_shamt		:	std_logic_vector(5-1 downto 0) := (others => '0');
+signal s_equal		:	std_logic := '0';
+signal s_overflow	:	std_logic := '0';
+signal s_result		:	std_logic_vector(32-1 downto 0) := (others => '0');
+signal s_carryout	:	std_logic := '0';
+signal s_signed		:	std_logic := '0';
+
+begin
+
+DUT0: ALU port map(
+	i_Adata		=>	s_Adata,
+	i_Bdata		=>	s_Bdata,
+	i_ALUShiftDir	=>	s_ALUShiftDir,
+	i_ALUShiftArithmetic	=>	s_ALUShiftArithmetic,
+	i_ALUAddSub	=>	s_ALUAddSub,
+	i_ALUMuxCtrl	=>	s_ALUMuxCtrl,
+	i_shamt		=>	s_shamt,
+	i_signed	=>	s_signed,
+	o_equal		=>	s_equal,
+	o_carryout	=>	s_carryout,			
+	o_overflow	=>	s_overflow,
+	o_result	=>	s_result);
+	
+ P_CLK: process
+  begin
+    s_CLK <= '0';
+    wait for gCLK_HPER;
+    s_CLK <= '1';
+    wait for gCLK_HPER;
+  end process;
+  
+  -- Testbench process  
+  P_TB: process
+  begin
+
+s_Adata		<= x"00000000";
+s_Bdata		<= x"000000FF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '0';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--00000000 && 000000FF
+s_Adata		<= x"00000000";
+s_Bdata		<= x"000000FF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '0';
+s_ALUMuxCtrl	<= "001";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+	
+--FFFFFFFF - 0000FFFF 
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"0000FFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--FFFFFFFF & 0000FFFF should be 0000FFFF
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"0000FFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "001";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--AAAAAAAA ^ 55555555 should be FFFFFFFF (1010 ^ 0101 = 1111)
+s_Adata		<= x"AAAAAAAA";
+s_Bdata		<= x"55555555";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "010";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--FFFFFFFF - FFFFFFFF should be zero, should output '1' for equal
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"0000FFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "010";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--FFFFFFFF - FFFFFFFF should be zero, should output '1' for equal
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"0000FFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+--FFFFFFFF - FFFFFFFF should be zero, should output '1' for equal bit
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"0000FFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "00000";
+
+wait for cCLK_PER;
+
+-- FFFFFFFF << 32 should be 00000000
+--Already tested the barrel shifter but can't be too safe >:}
+s_Adata		<= x"FFFFFFFF";
+s_Bdata		<= x"FFFFFFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "101";
+s_shamt		<= "11111";
+
+wait for cCLK_PER;
+
+-- 0000001 - 1FFFFFF0, should tell me that A<B and output 00000001
+s_Adata		<= x"00000001";
+s_Bdata		<= x"1FFFFFF0";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "110";
+s_shamt		<= "11111";
+
+wait for cCLK_PER;
+
+-- A = 000000AA... should RPL.QB to AAAAAAAA
+s_Adata		<= x"000000AA";
+s_Bdata		<= x"FFFFFFF0";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "111";
+s_shamt		<= "11111";
+
+wait for cCLK_PER;
+
+--------------------Now for some overflow tests-----------------------------------------
+-- Adding a big negative to a big negative should give overflow
+s_Adata		<= x"80000000";
+s_Bdata		<= x"80000000";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '0';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "11111";
+s_signed	<= '1';
+
+wait for cCLK_PER;
+
+--adding a big positive to a big positive should give an overflow
+s_Adata		<= x"7FFFFFFF";
+s_Bdata		<= x"7FFFFFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '0';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "11111";
+s_signed	<= '1';
+
+wait for cCLK_PER;
+
+--Subtractive a large negative by a large positive should produce overflow
+s_Adata		<= x"80000000";
+s_Bdata		<= x"7FFFFFFF";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "11111";
+s_signed	<= '1';
+
+wait for cCLK_PER;
+
+-----The following tests hopefully do NOT have overflow
+
+--sub small negative by small negative resulting in a positive
+s_Adata		<= x"FFFFFF23";
+s_Bdata		<= x"FFFF2345";
+s_ALUShiftDir	<= '0';	
+s_ALUShiftArithmetic	<= '0';
+s_ALUAddSub	<= '1';
+s_ALUMuxCtrl	<= "000";
+s_shamt		<= "11111";
+s_signed	<= '1';
+
+wait for cCLK_PER;
+
+
+
+
+	wait;
+	end process;
+
+end behavior;
+
+
+
+
---
+++ test/tb_MIPSFetch.vhd
@@ -0,0 +1,73 @@
+-------------------------------------------------------------------------
+-- Sidney Stowe
+-- CPR E 381
+-- Iowa State University
+-------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb_MIPSFetch is
+	generic(gCLK_HPER   : time := 10 ns); 
+end tb_MIPSFetch;
+
+architecture mixed of tb_MIPSFetch is
+
+constant cCLK_PER	:  time := gCLK_HPER * 2;
+
+component MIPSFetch is
+	port(i_PC	: in std_logic_vector(31 downto 0);
+	     i_Instr25t0	: in std_logic_vector(25 downto 0);
+	     i_ExtendedImm	: in std_logic_vector(31 downto 0);
+	     o_PC		: out std_logic_vector(31 downto 0);
+	     o_PCp8		: out std_logic_vector(31 downto 0);
+	     i_HALT	: in std_logic;
+	     i_CLK	: in std_logic;
+	     i_Jump	: in std_logic;
+	     i_Branch	: in std_logic;
+	     i_BranchNotEqual	: in std_logic;
+	     i_ALUResult	: in std_logic);
+end component;
+
+signal PC, imm, oPC, oPCp8	: std_logic_vector(31 downto 0);
+signal instr25t0	: std_logic_vector(25 downto 0);
+signal clk, jump, branch, bne, aluresult, halt	: std_logic;
+
+begin
+
+FETCHLOGIC: MIPSFetch port map(i_PC => PC, i_Instr25t0 => instr25t0, i_ExtendedImm => imm, o_PC => oPC, o_PCp8 => oPCp8, i_HALT => halt, i_CLK => clk, i_Jump => jump, i_Branch => branch, i_BranchNotEqual => bne, i_ALUResult => aluresult);
+
+	P_CLK: process
+	begin
+		clk <= '1';
+		wait for gCLK_HPER;
+		clk <= '0';
+		wait for gCLK_HPER;
+	end process;
+
+	P_TEST_CASES: process
+	begin
+		wait for gCLK_HPER;
+		PC <= x"00000000";
+		imm <= x"00000000";
+		halt <= '0';
+		instr25t0 <= "00000000000000000000000000";
+		jump <= '0';
+		branch <= '0';
+		bne <= '0';
+		aluresult <= '0';
+		wait for cCLK_PER;
+		PC <= x"00000004";
+		wait for cCLK_PER;
+		PC <= x"00000008";
+		wait for cCLK_PER;
+		halt <= '1';
+		wait for cCLK_PER;
+		PC <= x"0000000C";
+		wait for cCLK_PER;
+		PC <= x"00000010";
+		wait for cCLK_PER;
+		PC <= x"00000014";
+	end process;
+
+end mixed;
