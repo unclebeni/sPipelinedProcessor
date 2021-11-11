bf02edebcd0c7fa3b9b56f3054d083d873465030
sfstowe <sfstowe@co2050-03.ece.iastate.edu>
2021-11-11 15:57:39 -0600

    work on IF/ID

--- src_sw/IDEX.vhd
+++ src_sw/IDEX.vhd
@@ -3,3 +3,29 @@
 -- CPR E 381
 -- Iowa State University
 -------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+enitity IDEX is
+    port (
+
+    );
+end IDEX;
+
+architecture structural of IDEX is
+
+component Reg is
+    generic(N : integer := 32);
+	port(i_CLK	: in std_logic;
+	     i_RST	: in std_logic;
+	     i_WE	: in std_logic;
+	     i_D	: in std_logic_vector(N-1 downto 0);
+	     o_R	: out std_logic_vector(N-1 downto 0));
+end component;
+
+begin
+
+
+
+end structural;
