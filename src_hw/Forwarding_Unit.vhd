5b2943fe0fdca6cafa2e1d91b39da9262ae31979
crhunt <crhunt@co2050-01.ece.iastate.edu>
2021-12-02 16:03:05 -0600

    hw: forward: Setup hazard logic

--- src_hw/Forwarding_Unit.vhd
+++ src_hw/Forwarding_Unit.vhd
@@ -1,6 +1,6 @@
 ------------------------------------------------
 -- DESCRIPTION: This file contains the implementation for
--- Hardware based Pipeline Fowarding Unit
+-- Behavioral based Pipeline Fowarding Unit
 --
 -- AUTHOR: Cole Hunt
 -- CREATED: 11/18/21
@@ -12,15 +12,20 @@ use IEEE.std_logic_1164.all;
 entity ForwardingUnit is
 
     port(
-    i_RS        :
-    i_RT        :
-    i_EXMEM-RD  :
-    i_MEMWB-RD  :
-    o_ForwardA  : out -- Output to Signal 3 to 1 MUX  (ID/EX - luiMUX Output - EX/MEM -> ALU A)
+    i_IDEX_RS   : in std_logic_vector(4 downto 0);
+    i_IDEX_RT   : in std_logic_vector(4 downto 0);
+
+    i_EXMEM_RD  : in std_logic_vector(4 downto 0);
+    i_EXMEM_RW  : in std_logic_vector(4 downto 0);
+
+    i_MEMWB_RD  : in std_logic_vector(4 downto 0);
+    i_MEMWB_RW  : in std_logic_vector(4 downto 0);
+
+    o_ForwardA  : out std_logic_vector(1 downto 0) -- Output to Signal 3 to 1 MUX  (ID/EX - luiMUX Output - EX/MEM -> ALU A)
                         -- 00 - ID/EX - Comes from RegFile
                         -- 10 - EX/MEM - Forwarded from prior ALU result
                         -- 01 - MEM/WB - Forwarded from memory or earlier ALU result
-    o_ForwardB  : out -- Output to Signal 3 to 1 MUX  (ID/EX - luiMUX Output - EX/MEM -> Imm MUX for ALU)
+    o_ForwardB  : out std_logic_vector(1 downto 0) -- Output to Signal 3 to 1 MUX  (ID/EX - luiMUX Output - EX/MEM -> Imm MUX for ALU)
                         -- 00 - ID/EX - Comes from RegFile
                         -- 10 - EX/MEM - Forwarded from prior ALU result
                         -- 01 - MEM/WB - Forwarded from memory or earlier ALU result
@@ -28,6 +33,33 @@ entity ForwardingUnit is
 
 end ForwardingUnit;
 
-architecture struct of ForwardingUnit is
+architecture behavioral of ForwardingUnit is
 
-end struct;
+    -- No Hazard
+        o_ForwardA = "00";
+        o_ForwardB = "00";
+
+    -- EX Hazard
+        if (i_EXMEM_RW
+        and (i_EXMEM_RD != 0)
+        and (i_EXMEM_RD = i_IDEX_RS)) o_ForwardA = "10";
+
+        if (i_EXMEM_RW
+        and (i_EXMEM_RD != 0)
+        and (i_EXMEM_RD = i_IDEX_RT)) o_ForwardB = "10";
+
+    -- MEM Hazard
+        if (i_MEMWB_RW
+        and (i_MEMWB_RD != 0)
+        and not(i_EXMEM_RW and (i_EXMEM_RD != 0)
+            and (i_EXMEM_RD != i_IDEX_RS))
+        and (i_MEMWB_RD = i_IDEX_RS)) o_ForwardA = "01";
+
+        if (i_MEMWB_RW
+        and (i_MEMWB_RD != 0)
+        and not(i_EXMEM_RW and (i_EXMEM_RD != 0)
+            and (i_EXMEM_RD != i_IDEX_RT))
+        and (i_MEMWB_RD = i_IDEX_RT)) o_ForwardB = "01";
+
+
+end behavioral;
