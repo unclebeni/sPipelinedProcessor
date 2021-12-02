276d12d0ba470e5f0f72271706e67b418491fd7a
bmoeckly <bmoeckly@co2050-02.ece.iastate.edu>
2021-12-02 16:02:31 -0600

    work

--- src_hw/hazardDetection.vhd
+++ src_hw/hazardDetection.vhd
@@ -10,18 +10,35 @@ use IEEE.std_logic_1164.all;
 
 entity hazardDetection is 
     port(
-    i_currentInst   : in std_logic_vector(32-1 downto 0);
-    i_incomingInst  : in std_logic_vector(32-1 downto 0);
-    o_RAWhazard     : out std_logic;
-    o_instruction_out : out std_logic_vector(32-1 downto 0);
-    o_BRANCHhazard  : out std_logic);
+    i_FetchInst         : in std_logic_vector(32-1 downto 0);
+    i_DecodeInst        : in std_logic_vector(32-1 downto 0);
+    i_ExectuteInst      : in std_logic_vector(32-1 downto 0);
+    i_branchNotTaken    : in std_logic; 
+    o_RAWhazard         : out std_logic;
+    o_instruction_out   : out std_logic_vector(32-1 downto 0);
+    o_Flush             : out std_logic;
+    o_Stall             : out std_logic;  
+    o_SecondStall       : out std_logic;
+    o_BRANCHhazard      : out std_logic);
 
     end hazardDetection;
 
     architecture behavior of hazardDetection is 
 
+
+
     begin
 
+    ---First I want to cover all of the RAW Hazards and implement stalls as needed.
+
+            
+            
 
 
-        end behavior;
+            o_Stall <= 
+                             --^^^CASE 1: Both Instructions are R format (opcode 000000), currentInst Rd = incomingInst rt
+                 --^^^CASE 2: Instructions are R format, currentINST Rd = incomingINST rs^^^
+                 --^^^CASE 3: 
+            '0'; --Base case, no stall.
+
+    end behavior;
