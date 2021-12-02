a9216a4c21bec2da2afeef4d06f3687be0ea0a75
bmoeckly <bmoeckly@vlinux-20.ece.iastate.edu>
2021-12-02 12:50:25 -0600

    sw tests, hazard detection work

--- mips_sw/Proj1_bubblesort.s
+++ mips_sw/Proj1_bubblesort.s
@@ -5,18 +5,15 @@
 #say the array size is located in $s1
 
 #this is the main body of the bubble sort algorithm implementation
+.data
+
+array: .word 3, -1, 2, -4
+
+
 
 lui $s0, 0x1003
-addi $t0, $zero, -3
-addi $t1, $zero, 3
-addi $t2, $zero, 2
-addi $t3, $zero,  -4
 
-addi $s1, $zero, 4
-sw $t0, (0)$s0
-sw $t1, (4)$s0
-sw $t2, (8)$s0
-sw $t3, (12)$s0
+la $s0, array
 
 #initialize the array ^^^^^^^^^^
 
@@ -25,7 +22,7 @@ sw $t3, (12)$s0
 addu $t1 $zero, $zero	#initialize i
 addi $t7 $t7, -1		#t7 will be n-1
 
-jump loop1:
+j loop1:
 
 loop0:
 
@@ -34,23 +31,40 @@ addi $t1, $t1, 1
 loop1:
 	addu $t6, $zero, $zero   #j = 0
 	addu $t2, $s0, $zero #t2 = &arr
+	
+	add $zero, $zero, $zero #nop
+
 	slt $t2, $t1, $t7 
 	beq $t2, $zero exit(?) #while  i < n-1	
+
+	add $zero, $zero, $zero #nop  
 
 loop2:  
 
 	sub $t5. $t7, $t1  # n - i - 1
 	slt $t2, $t6, $t5
+
+	add $zero, $zero, $zero #nop
+
 	bne $t2, $zero loop0	#while j < n-1-i 
+
+	add $zero, $zero, $zero #nop
+
 	addu $t2, $s0, $t6  #current array address... &arr + j
 
 	lw $t3, (0)$t2 #load array [j]
 	lw $t4, (4)$t2 #load array [j+1]
 
-	slt $t2. $t3, $t4 
+	slt $t2, $t3, $t4 
 	beq $t2, $zero point #if t4 >= t3 then start the loop again, otherwise swap then loop
+
+	add $zero, $zero, $zero #nop
+
 	add $t2, $t4, $zero 
 	add $t4, $t3, $zero  #swap
+
+	add $zero, $zero, $zero #nop	
+
 	add $t3, $t2, $zero
 
 point: 
--- src_hw/branchPredict.vhd
+++ src_hw/branchPredict.vhd
@@ -10,5 +10,29 @@ use IEEE.std_logic_1164.all;
 
 entity branchPrediction is 
     port(
-    i_enable    : in std_logic
+    i_taken		: in std_logic;
+    i_enable    	: in std_logic;
+    o_preditction	: out std_logic;
     );
+
+	end branchPrediction;
+
+architecture struct of branchPrediction is
+
+--I think we will need a flipflop component to store the state of the branch prediction unit
+component dffg is
+
+  port(i_CLK        : in std_logic;     -- Clock input
+       i_RST        : in std_logic;     -- Reset input
+       i_WE         : in std_logic;     -- Write enable input
+       i_D          : in std_logic;     -- Data value input
+       o_Q          : out std_logic);   -- Data value output
+
+end component;
+
+
+
+
+begin
+
+end struct;
